34147aa6496d570d9a4c2a3fd4ef8014f355cc4c
StoneyDSP <nathanjhood@googlemail.com>
2024-02-18 03:28:15 +0100

    Added 'version.h'

---
+++ include/StoneyDSP/version.h
@@ -0,0 +1,16 @@
+/**
+ * @file version.h
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2024-02-18
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+
+#ifndef STONEYDSP_VERSION_H
+#define STONEYDSP_VERSION_H
+
+#endif // STONEYDSP_VERSION_H
