91724afe41b21566f32dc2d046c505cb1e52c2c0
StoneyDSP <nathanjhood@googlemail.com>
2024-02-18 05:16:18 +0100

    Created 'StoneyDSP' namespace

---
+++ include/StoneyDSP/StoneyDSP.h
@@ -0,0 +1,23 @@
+/**
+ * @file StoneyDSP.h
+ * @author Nathan J. Hood (nathanjhood@googlemail.com)
+ * @brief
+ * @version 0.1
+ * @date 2024-02-18
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#ifndef STONEYDSP_STONEYDSP_H
+#define STONEYDSP_STONEYDSP_H
+
+/**
+ * @brief The ```StoneyDSP``` namespace.
+ * @author Nathan J. Hood (nathanjhood@googlemail.com)
+ * @copyright Copyright (c) 2024
+ *
+ */
+namespace StoneyDSP {}
+
+#endif // STONEYDSP_STONEYDSP_H
