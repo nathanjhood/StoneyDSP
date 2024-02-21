5dde79f0417e0691cf31378eae5cd231da7c5b42
StoneyDSP <nathanjhood@googlemail.com>
2024-02-22 00:13:53 +0100

    Improved license info

--- modules/stoneydsp_core/version/stoneydsp_version.h
+++ modules/stoneydsp_core/version/stoneydsp_version.h
@@ -1,6 +1,6 @@
 /***************************************************************************//**
  * @file stoneydsp_version.h
- * @author StoneyDSP (nathanjhood@googlemail.com)
+ * @author Nathan J. Hood (nathanjhood@googlemail.com)
  * @brief Versions with the same major number are ABI stable. API is allowed to
  * evolve between minor releases, but only in a backwards compatible way.
  * Make sure you update the -soname directives in configure.ac
@@ -9,9 +9,30 @@
  * @version 0.1
  * @date 2023-09-09
  *
- * @copyright Copyright (c) 2023
+ * @copyright Copyright (c) 2024
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  *
  ******************************************************************************/
+
 #pragma once
 
 #define STONEYDSP_CORE_VERSION_H_INCLUDED
