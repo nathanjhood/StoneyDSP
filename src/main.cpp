9120b4eeb6430e59c4a2d02991d6b685067027c2
StoneyDSP <nathanjhood@googlemail.com>
2024-02-21 22:09:48 +0100

    Added initial 'main' source/header for tests/debugging

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+/***************************************************************************//**
+ * @file main.cpp
+ * @author StoneyDSP (nathanjhood@googlemail.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-09-09
+ *
+ *
+ * @copyright Copyright (c) 2023
+ *
+ ******************************************************************************/
+
+// #include "StoneyDSP/modules/stoneydsp_core/stoneydsp_core.cpp"
+
+
+#include "main.h"
+
+#include <iostream>
+
+int Main(int argc, char* argv[])
+{
+
+    for(int i = 0; i < argc; ++i)
+        std::cout << "Argument " << i << ": " << argv[i] << std::endl;
+
+    std::cout << std::endl;
+
+    std::cin.get();
+
+    return 0;
+}
