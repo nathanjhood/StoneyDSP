092f4ca7b6f2a588a124c514b280b55bc7944e38
StoneyDSP <nathanjhood@googlemail.com>
2024-02-21 22:05:45 +0100

    Started 'stoneydsp' module

---
+++ modules/stoneydsp.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file stoneydsp.cpp
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2024-02-21
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include "stoneydsp.h"
