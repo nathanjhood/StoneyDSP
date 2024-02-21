b16cfbb3f0ace555eeca586e359f5661bcdb690d
StoneyDSP <nathanjhood@googlemail.com>
2024-02-21 22:07:27 +0100

    Enhanced 'stoneydsp_core' module

---
+++ modules/stoneydsp_core/types/stoneydsp_types.h
@@ -0,0 +1,14 @@
+/**
+ * @file stoneydsp_types.h
+ * @author StoneyDSP (nathanjhood@googlemail.com)
+ * @brief
+ * @version 0.1
+ * @date 2024-02-21
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#pragma once
+
+#define __STONEYDSP_TYPES_H__
