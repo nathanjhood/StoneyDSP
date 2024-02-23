1b51837bfaa860efcd637dbde9c48698183fd7c1
StoneyDSP <nathanjhood@googlemail.com>
2024-02-23 11:07:05 +0100

    Fixed an ECMAScript binding

---
+++ index.js
@@ -0,0 +1,4 @@
+// This small codeblock in your root-level index.js allows others to consume
+// StoneyDSP as any other NodeJS module
+const StoneyDSP = require(`./lib/stoneydsp`);
+module.exports = StoneyDSP;
