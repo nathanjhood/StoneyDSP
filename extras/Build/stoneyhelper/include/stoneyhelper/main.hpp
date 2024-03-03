e6701f93e831f4e8eda3de0210c2bedaaa6e1a2f
Nathan <nathanjhood@googlemail.com>
2024-03-03 03:32:03 +0100

    Development/v0.0.1-rc (#23)
    * Merge 'production' v.0.0.1 into 'preview' (#11) (#12)
    
    * Revert "Release v0.0.0"
    
    This reverts commit 1ce75cf7fbc08af18239a78d3d8130b9538a6e56.
    
    * Release v0.0.0
    
    * Added contributing, code of conduct, and issue/PR templates to '.github' dir
    
    * Update cmake_project.yaml
    
    
    
    * Release v0.0.1
    
    * Release v0.0.1
    
    
    
    * Release fix v0.0.1
    
    
    
    ---------
    
    Signed-off-by: Nathan <nathanjhood@googlemail.com>
    
    * Improved CI/CD logic
    
    Signed-off-by: Nathan <nathanjhood@googlemail.com>
    
    * Create dependabot.yml
    
    Signed-off-by: Nathan <nathanjhood@googlemail.com>
    
    * Removed .clang-tidy
    
    * Enhanced .gitignore
    
    * Added 'vcpkg-configuration.json'
    
    * Removed '.vscode/*' unneeded files
    
    * Enhanced IDE configuration files
    
    * Enhanced .gitignore
    
    * Improved CPack ignore list - might make this an external file...
    
    * Moved ignore list to a unique file for sharing
    
    * Added JUCE to git submodules
    
    * Added vcpkg to git submodules
    
    * Added CMake as a git submodule
    
    * Added ConsoleExample to .gitignore for now
    
    * Improved vcpkg dependencies for different platforms instead of all as one
    
    * Added default feature flags
    
    * Improved CPack output strings
    
    * Added TLS certs to ignore lists
    
    * Improved some IDE environment vars for CMake tools
    
    * Cleanup for release - stoneyhelper and GUI App ready for testing
    
    * v0.0.1-rc
    
    ---------
    
    Signed-off-by: Nathan <nathanjhood@googlemail.com>

---
+++ extras/Build/stoneyhelper/include/stoneyhelper/main.hpp
@@ -0,0 +1,15 @@
+/**
+ * @file main.h
+ * @author Nathan J. Hood (nathanjhood@googlemail.com)
+ * @brief
+ * @version 0.1
+ * @date 2024-02-27
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#ifndef STONEYDSP_MAIN_H
+#define STONEYDSP_MAIN_H
+
+#endif //STONEYDSP_MAIN_H
