5c93bf4db350efab80763a7983f631a1011d33d8
StoneyDSP <nathanjhood@googlemail.com>
2025-01-30 18:07:52 +0100

    add 'src/stoneydsp/stoneydsp.cpp'
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

---
+++ src/stoneydsp/stoneydsp.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file stoneydsp.cpp
+ * @author StoneyDSP (nathanjhood@googlemail.com)
+ * @copyright Copyright (c) 2025
+ *
+ */
+
+//==============================================================================
+
+#ifdef STONEYDSP_H_INCLUDED
+	#error wrong inclusion of stoneydsp.h
+#endif
+
+#include "stoneydsp/stoneydsp.h"
+
+//==============================================================================
