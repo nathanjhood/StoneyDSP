0eeac64f84c9f480eb1e28d6c5b84d16441b42a8
StoneyDSP <nathanjhood@googlemail.com>
2025-01-30 18:06:53 +0100

    add 'src/stoneydsp/core/core.cpp'
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

---
+++ src/stoneydsp/core/core.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file core.cpp
+ * @author StoneyDSP (nathanjhood@googlemail.com)
+ * @copyright Copyright (c) 2025
+ *
+ */
+
+//==============================================================================
+
+#ifdef STONEYDSP_CORE_H_INCLUDED
+	#error wrong inclusion of stoneydsp/core/core.h
+#endif
+
+#include "stoneydsp/core/core.h"
+
+//==============================================================================
