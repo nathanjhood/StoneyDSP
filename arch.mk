ced47dd86aec922a75041a55171856a3ea71ca00
StoneyDSP <nathanjhood@googlemail.com>
2025-01-30 17:40:25 +0100

    edit 'arch.mk' - added CMake preset handling
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- arch.mk
+++ arch.mk
@@ -31,3 +31,45 @@ $(error Could not determine operating system of $(MACHINE))
 endif
 
 ARCH_NAME = $(ARCH_OS)-$(ARCH_CPU)
+
+ifdef ARCH_X64
+	TRIPLET_ARCH := x64
+	PRESET_ARCH := x64
+endif
+
+ifdef ARCH_ARM64
+	TRIPLET_ARCH := arm64
+	PRESET_ARCH := arm64
+endif
+
+# Include deps
+ifdef ARCH_WIN
+	TRIPLET_OS := mingw-dynamic
+	PRESET_OS := windows
+endif
+
+ifdef ARCH_LIN
+	TRIPLET_OS := linux
+	PRESET_OS := linux
+endif
+
+ifdef ARCH_MAC
+	TRIPLET_OS := osx
+	PRESET_OS := osx
+endif
+
+ifdef BUILD_TYPE
+	PRESET_CONFIG := $(BUILD_TYPE)
+else
+	ifdef DEBUG
+		PRESET_CONFIG := debug
+	else
+		PRESET_CONFIG := release
+	endif
+endif
+
+ifdef VERBOSE
+	PRESET_VERBOSE := -verbose
+endif
+
+PRESET ?= $(PRESET_ARCH)-$(PRESET_OS)-$(PRESET_CONFIG)$(PRESET_VERBOSE)
