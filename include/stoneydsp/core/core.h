c3e302f6c59a08a57397f3c2cae18ad5533d789d
StoneyDSP <nathanjhood@googlemail.com>
2025-01-30 18:03:30 +0100

    add 'include/stoneydsp/core/core.hpp'
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

---
+++ include/stoneydsp/core/core.h
@@ -0,0 +1,32 @@
+/**
+ * @file core.h
+ * @author StoneyDSP (nathanjhood@googlemail.com)
+ * @brief
+ * @version @STONEYDSP_CORE_VERSION@
+ * @date 2025-01-27
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+
+#ifndef STONEYDSP_CORE_H_INCLUDED
+#define STONEYDSP_CORE_H_INCLUDED
+
+//====================================================================//INCLUDES
+
+#include "stoneydsp/core/system/platform.h"
+#include "stoneydsp/core/system/compiler.h"
+#include "stoneydsp/core/system/languages.h"
+#include "stoneydsp/core/system/cxx.h"
+
+//==============================================================================
+
+#include "stoneydsp/core/types/generic.h"
+#include "stoneydsp/core/types/int.h"
+#include "stoneydsp/core/types/math.h"
+
+//==============================================================================
+
+#endif // STONEYDSP_CORE_H_INCLUDED
+
+//=========================================================================//EOF
