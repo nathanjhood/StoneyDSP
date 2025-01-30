5ff11453992d1bab7ce2fd1a32f22c6cce426584
StoneyDSP <nathanjhood@googlemail.com>
2025-01-30 17:40:48 +0100

    add 'dep.mk'
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

---
+++ dep.mk
@@ -0,0 +1,72 @@
+GIT := git
+ZIP := ZIP
+UNZIP := unzip
+
+include ./arch.mk
+
+# Fetch submodules
+submodules:
+	$(shell $(GIT) submodule update --init --recursive)
+
+.PHONY: submodules
+
+# Fetch vcpkg
+./dep/vcpkg: submodules
+
+# Bootstrap vcpkg
+./dep/vcpkg/bootstrap-vcpkg.sh: ./dep/vcpkg
+
+# Use vcpkg
+./dep/vcpkg/vcpkg: ./dep/vcpkg/bootstrap-vcpkg.sh
+
+.PHONY: ./dep/vcpkg ./dep/vcpkg/bootstrap-vcpkg.sh ./dep/vcpkg/vcpkg
+
+VCPKG_ROOT ?= ./dep/vcpkg
+VCPKG := $(VCPKG_ROOT)/vcpkg
+
+STONEYDSP_EXTERNAL_DEPS :=
+# EXTERNAL_DEPS += Rack-SDK
+STONEYDSP_EXTERNAL_DEPS += catch2
+
+PKG_CONFIG_PATH += build/vcpkg_installed/$(TRIPLET_ARCH)-$(TRIPLET_OS)/lib/pkgconfig
+
+LIB_CATCH := build/vcpkg_installed/$(TRIPLET_ARCH)-$(TRIPLET_OS)/lib/libCatch2.a
+
+FLAGS += -Lbuild/vcpkg_installed/$(TRIPLET_ARCH)-$(TRIPLET_OS)/lib
+CFLAGS += -Ibuild/vcpkg_installed/$(TRIPLET_ARCH)-$(TRIPLET_OS)/include
+CXXFLAGS += -Ibuild/vcpkg_installed/$(TRIPLET_ARCH)-$(TRIPLET_OS)/include
+CPPFLAGS += -Ibuild/vcpkg_installed/$(TRIPLET_ARCH)-$(TRIPLET_OS)/include
+
+# ifdef STONEYDSP_BUILD_TEST
+# 	LDFLAGS += -Ibuild/vcpkg_installed/$(TRIPLET_ARCH)-$(TRIPLET_OS)/include
+# 	LDFLAGS += -Lbuild/vcpkg_installed/$(TRIPLET_ARCH)-$(TRIPLET_OS)/lib
+# 	LDFLAGS += -lCatch2
+# endif
+
+# ifneq ($(strip $(shell PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) pkg-config --cflags-only-I $(STONEYDSP_EXTERNAL_DEPS) 2>/dev/null)),)
+# 	STONEYDSP_CFLAGS += -isystem $(strip $(shell PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) pkg-config --cflags-only-I $(STONEYDSP_EXTERNAL_DEPS) 2>/dev/null))
+# endif
+# ifneq ($(strip $(shell PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) pkg-config --cflags-only-other $(STONEYDSP_EXTERNAL_DEPS) 2>/dev/null)),)
+# 	STONEYDSP_CFLAGS += -isystem $(strip $(shell PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) pkg-config --cflags-only-other $(STONEYDSP_EXTERNAL_DEPS) 2>/dev/null))
+# endif
+# ifneq ($(strip $(shell PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) pkg-config --cflags-only-I $(STONEYDSP_EXTERNAL_DEPS) 2>/dev/null)),)
+# 	STONEYDSP_CXXFLAGS += -isystem $(strip $(shell PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) pkg-config --cflags-only-I $(STONEYDSP_EXTERNAL_DEPS) 2>/dev/null))
+# endif
+# ifneq ($(strip $(shell PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) pkg-config --cflags-only-other $(STONEYDSP_EXTERNAL_DEPS) 2>/dev/null)),)
+# 	STONEYDSP_CXXFLAGS += -isystem $(strip $(shell PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) pkg-config --cflags-only-other $(STONEYDSP_EXTERNAL_DEPS) 2>/dev/null))
+# endif
+# ifneq ($(strip $(shell PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) pkg-config --cflags-only-I $(STONEYDSP_EXTERNAL_DEPS) 2>/dev/null)),)
+# 	STONEYDSP_CPPFLAGS += -isystem $(strip $(shell PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) pkg-config --cflags-only-I $(STONEYDSP_EXTERNAL_DEPS) 2>/dev/null))
+# endif
+# ifneq ($(strip $(shell PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) pkg-config --cflags-only-other $(STONEYDSP_EXTERNAL_DEPS) 2>/dev/null)),)
+# 	STONEYDSP_CPPFLAGS += -isystem $(strip $(shell PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) pkg-config --cflags-only-other $(STONEYDSP_EXTERNAL_DEPS) 2>/dev/null))
+# endif
+# ifneq ($(strip $(shell PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) pkg-config --libs-only-l $(STONEYDSP_EXTERNAL_DEPS) 2>/dev/null)),)
+# 	STONEYDSP_LDFLAGS += -isystem $(strip $(shell PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) pkg-config --libs-only-l $(STONEYDSP_EXTERNAL_DEPS) 2>/dev/null))
+# endif
+# ifneq ($(strip $(shell PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) pkg-config --libs-only-L $(STONEYDSP_EXTERNAL_DEPS) 2>/dev/null)),)
+# 	STONEYDSP_LDFLAGS += -isystem $(strip $(shell PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) pkg-config --libs-only-L $(STONEYDSP_EXTERNAL_DEPS) 2>/dev/null))
+# endif
+# ifneq ($(strip $(shell PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) pkg-config --libs-only-other $(STONEYDSP_EXTERNAL_DEPS) 2>/dev/null)),)
+# 	STONEYDSP_LDFLAGS += -isystem $(strip $(shell PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) pkg-config --libs-only-other $(STONEYDSP_EXTERNAL_DEPS) 2>/dev/null))
+# endif
