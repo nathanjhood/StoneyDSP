990f9f911a684669d3547ac308f3615ae8319bad
StoneyDSP <nathanjhood@googlemail.com>
2025-01-31 03:38:35 +0100

    edit 'include/stoneydsp.h' - remove duplicate macros
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- include/stoneydsp/stoneydsp.h
+++ include/stoneydsp/stoneydsp.h
@@ -14,17 +14,6 @@
 
 //==============================================================================
 
-#ifndef STONEYDSP_STRINGIFY
-  # define STONEYDSP_STRINGIFY_HELPER(n) #n
-  /**
-   * @brief Handy stringify pre-processor macro.
-   * @param n The value to stringify.
-   */
-  # define STONEYDSP_STRINGIFY(n) STONEYDSP_STRINGIFY_HELPER(n)
-#endif
-
-//==============================================================================
-
 #ifdef STONEYDSP_BUILD_CORE
 	#include "stoneydsp/core/core.h"
 #endif
@@ -40,23 +29,6 @@
 #ifdef STONEYDSP_BUILD_DSP
 // #include "stoneydsp/dsp/dsp.h"
 #endif
-
-//==============================================================================
-
-// Define wide character support macros
-#if STONEYDSP_WINDOWS
-  #define STONEYDSP_SUPPORTS_WIDECHARS 1
-#else
-  #if __cplusplus >= 201103L || (__GNUC__ >= 5)
-    #define STONEYDSP_SUPPORTS_WIDECHARS 1
-  #endif
-#endif
-
-//==============================================================================
-
-// #if STONEYDSP_ARM && STONEYDSP_USING_INT64_2 != 0
-//  #define STONEYDSP_USING_INT64_2 1
-// #endif
 
 //==============================================================================
 
