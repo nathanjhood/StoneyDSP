2879e45154264adc10ffb7c8301c3ebcabf91ed2
StoneyDSP <nathanjhood@googlemail.com>
2025-01-31 02:53:40 +0100

    edit '{include,src}/stoneydsp/core/core.{hpp,cpp}' - add version number from pre-processor defines
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- include/stoneydsp/core/core.h
+++ include/stoneydsp/core/core.h
@@ -12,6 +12,47 @@
 #ifndef STONEYDSP_CORE_H_INCLUDED
 #define STONEYDSP_CORE_H_INCLUDED
 
+//============================================================================//
+
+/**
+ * @brief Internal helper for stringify pre-processor macro.
+ * @param n The value to stringify.
+ */
+# define __STONEYDSP_STRINGIFY_HELPER(n) #n
+
+/**
+ * @brief Handy stringify pre-processor macro.
+ * @param n The value to stringify.
+ */
+# define STONEYDSP_STRINGIFY(n) __STONEYDSP_STRINGIFY_HELPER(n)
+
+/* Convert integer to decimal digit literals.  */
+#define STONEYDSP_DEC(n)         \
+  ('0' + (((n) / 10000000)%10)), \
+  ('0' + (((n) / 1000000)%10)),  \
+  ('0' + (((n) / 100000)%10)),   \
+  ('0' + (((n) / 10000)%10)),    \
+  ('0' + (((n) / 1000)%10)),     \
+  ('0' + (((n) / 100)%10)),      \
+  ('0' + (((n) / 10)%10)),       \
+  ('0' +  ((n) % 10))
+
+/* Convert integer to hex digit literals.  */
+#define STONEYDSP_HEX(n)   \
+  ('0' + ((n)>>28 & 0xF)), \
+  ('0' + ((n)>>24 & 0xF)), \
+  ('0' + ((n)>>20 & 0xF)), \
+  ('0' + ((n)>>16 & 0xF)), \
+  ('0' + ((n)>>12 & 0xF)), \
+  ('0' + ((n)>>8  & 0xF)), \
+  ('0' + ((n)>>4  & 0xF)), \
+  ('0' + ((n)     & 0xF))
+
+//============================================================================//
+
+/* tring literal encoding the version number. */
+extern char const* stoneydsp_version;
+
 //====================================================================//INCLUDES
 
 #include "stoneydsp/core/system/platform.h"
--- src/stoneydsp/core/core.cpp
+++ src/stoneydsp/core/core.cpp
@@ -14,3 +14,27 @@
 #include "stoneydsp/core/core.h"
 
 //==============================================================================
+
+/* Construct a string literal encoding the version number. */
+#ifdef STONEYDSP_VERSION
+char const* stoneydsp_version = "INFO" ":" "StoneyDSP version [" STONEYDSP_STRINGIFY(STONEYDSP_VERSION) "]";
+
+/* Construct a string literal encoding the version number components. */
+#elif defined(STONEYDSP_VERSION_MAJOR)
+char const stoneydsp_version[] = {
+  'I', 'N', 'F', 'O', ':',
+  'S','t','o','n','e','y','D','S','P','_','v','e','r','s','i','o','n','_','[',
+  STONEYDSP_STRINGIFY(STONEYDSP_VERSION_MAJOR),
+# ifdef STONEYDSP_VERSION_MINOR
+  '.', STONEYDSP_STRINGIFY(STONEYDSP_VERSION_MINOR),
+#  ifdef STONEYDSP_VERSION_PATCH
+   '.', STONEYDSP_STRINGIFY(STONEYDSP_VERSION_PATCH),
+#   ifdef STONEYDSP_VERSION_TWEAK
+    '.', STONEYDSP_STRINGIFY(STONEYDSP_VERSION_TWEAK),
+#   endif
+#  endif
+# endif
+  ']','\0'};
+#else
+char const* stoneydsp_version = "INFO" ":" "StoneyDSP version [UNKNOWN]";
+#endif
