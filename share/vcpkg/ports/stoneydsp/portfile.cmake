c4af7328327d4e3e2a70e2bd57141d2d23921725
StoneyDSP <nathanjhood@googlemail.com>
2025-02-10 23:31:26 +0100

    add 'share/vcpkg/ports/stoneydsp/portfile.cmake'
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

---
+++ share/vcpkg/ports/stoneydsp/portfile.cmake
@@ -0,0 +1,29 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO StoneyDSP/StoneyDSP
+    REF 2f9abd0dc94a764923d6ec22ba57840660c57794
+    SHA512 e96c934f43eca898c18691cab89584b0f6fd3119f1c072fbe389a400967a6d0c52f931ff409f9579d4fcc9dfc491e403abb881d8c31941084ea540b220345335
+    HEAD_REF feat/stoneydsp
+)
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+    core STONEYDSP_BUILD_CORE
+)
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+    "-DSTONEYDSP_GENERATE_VERSIONS:BOOL=OFF"
+    ${FEATURE_OPTIONS}
+)
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(
+    PACKAGE_NAME stoneydsp
+    CONFIG_PATH "lib/cmake/stoneydsp"
+)
+vcpkg_fixup_pkgconfig()
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(
+    INSTALL "${SOURCE_PATH}/LICENSE"
+    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+    RENAME copyright
+)
