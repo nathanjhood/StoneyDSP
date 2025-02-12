b52d0139522ef305c4062a2c6ba4f2c8c992bacc
StoneyDSP <nathanjhood@googlemail.com>
2025-02-12 03:21:54 +0100

    add '{include,src}/stoneydsp/simd/simd.{cpp,hpp}'
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

---
+++ include/stoneydsp/simd/simd.h
@@ -0,0 +1,21 @@
+/**
+ * @file simd.h
+ * @author StoneyDSP (nathanjhood@googlemail.com)
+ * @brief
+ * @version @STONEYDSP_SIMD_VERSION@
+ * @date 2025-01-27
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+
+#pragma once
+
+#ifndef STONEYDSP_SIMD_H_INCLUDED
+  #define STONEYDSP_SIMD_H_INCLUDED
+
+  #include "stoneydsp/simd/vector/support.h"
+
+// #include "stoneydsp/simd/vector/vector.h"
+
+#endif // STONEYDSP_SIMD_H_INCLUDED
